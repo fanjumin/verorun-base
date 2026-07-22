0d04650e1e3d4026f0683155e44bd9c0cfe17a58
fanjumin <guxiao@yahoo.com>
2026-07-22 16:30:06 +0800

    fix: workflow-editor React Flow NaN  container 改用 absolute 定位避免 0x0 初始化

--- admin/static/js/workflow_editor_app.js
+++ admin/static/js/workflow_editor_app.js
@@ -396,8 +396,11 @@ function FlowEditor() {
   }, [undo, redo]);
   return E('div', {
     style: {
-      width: '100%',
-      height: '100%'
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
     },
     ref: reactFlowWrapper
   }, E(Provider, null, E(RF, {
